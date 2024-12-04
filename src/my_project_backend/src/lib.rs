48666d147bd71672ae465ea27253a8f0e5da825e
Dominik Kasza <mannakasza1@gmail.com>
2024-12-04 10:42:03 +0000

    updated frontend

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,26 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn add_msg(new_msg: String) {
+    CHAT.with(|chat| {
+        chat.borrow_mut().push(new_msg)
+    })
+}
+
+#[ic_cdk::query]
+fn get_chat() -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
+    let a = String::from("Siema");
+    let b = &a;
+    let c = a;
+
     format!("Hello, {}!", name)
 }
