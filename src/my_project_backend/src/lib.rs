b5fe2a33d235e8586e0d629ff7c5583f9cccbb72
Grengorio <mannakasza1@gmail.com>
2024-12-04 10:49:04 +0100

    Update lib.rs

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,30 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| {
+        *msg.borrow_mut() = new_msg
+    })
+}
+
+#[ic_cdk::query]
+fn get_msg(name: String) -> String {
+    let a = String::from("Siema");
+    let b = &a;
+    let c = a;
+
+    format!("Hello, {}!", name)
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
+    let a = String::from("Siema");
+    let b = &a;
+    let c = a;
+
     format!("Hello, {}!", name)
 }
